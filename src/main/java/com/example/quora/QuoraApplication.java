22eb4df8f3b04c4b6e5d15ff7e2cbf030ad9bb8d
soham023 <soham.sadhukhan03@gmail.com>
2026-01-31 23:31:47 +0530

    created Quora App Project for the very 1st time.

---
+++ src/main/java/com/example/quora/QuoraApplication.java
@@ -0,0 +1,14 @@
+package com.example.quora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(QuoraApplication.class, args);
+	}
+
+}
