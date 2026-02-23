68c7a761228dc4e2c872d5389dd476a1f8e04bfe
soham023 <soham.sadhukhan03@gmail.com>
2026-02-24 03:44:56 +0530

    kafka 1st step

--- src/main/java/com/example/quora/configurationsKafka/KafkaConfig.java
+++ src/main/java/com/example/quora/configurationsKafka/KafkaConfig.java
@@ -1,14 +1,16 @@
 package com.example.quora.configurationsKafka;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.BytesDeserializer;
 import org.apache.kafka.common.serialization.BytesSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -28,6 +30,8 @@ public class KafkaConfig {
     @Value("${spring.kafka.consumer.group-id:view-count-consumer}")
     private String groupId;
 
+    public static final String TOPIC_NAME = "view-count-topic";
+
     @Bean
     // ProducerFactory ?
     // Generally Factory is referred to a class which has the responsibility of creation of an object
@@ -40,16 +44,45 @@ public class KafkaConfig {
         // now you are going to decide all the kafka related properties
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         // key serialiser
+        // When you are dumping your data into kafka -> you need to serialise your data.
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         // value serialiser
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, BytesSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(configProps);
 
+    }
 
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        // we'll create a map where we'll store all the kafka configurations
+        Map<String, Object> configProps = new HashMap<>();
+
+        // now you are going to decide all the kafka related properties
+        // when you are reading from kafka, you need to deserialise your data.
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        // key deserialiser
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        // value deserialiser
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, BytesDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
 
     }
 
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        return factory;
+    }
 
 //    @Bean
 //    public JsonMessageConverter jsonMessageConverter() {
